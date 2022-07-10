9c95c12d465eb64b38cbbd93f653ebc3e1671fdc
tetianaborysova <tetiana.borysova@empeek.io>
2022-07-10 14:53:26 +0200

    Initial Commit

---
+++ ImageRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRecognizer
+//
+//  Created by Tetiana Borysova on 10.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
